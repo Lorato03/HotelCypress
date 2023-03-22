20828bc16b292c76de6be77d1bc05868413d78ac
Lorato03 <leratot@qotsolutions.co.za>
2023-03-22 17:12:28 +0200

    cypress hotel project

---
+++ Hotel_Cypress/cypress/page elements/elements.js
@@ -0,0 +1,14 @@
+export const elements = {
+    link: "http://hotel-test.equalexperts.io/",
+    firstN: "#firstname",
+    lastN: "#lastname",
+    price: "#totalprice",
+    depo: "#depositpaid",
+    checkIn: "#checkin",
+    checkOut: "#checkout",
+    saveBTN: "#form > div > div:nth-child(7) > input[type=button]",
+    dlt: ":nth-child(7) > input",
+    rows: "#bookings > div.row",
+    tableCell: "p",
+    
+  };
---
+++ Hotel_Cypress/cypress/pages/landing.js
@@ -0,0 +1,38 @@
+import { elements } from "../page elements/elements";
+
+class HomePage{
+    site(){
+        cy.visit(elements.link);
+    }
+
+    //fill in the booking form
+
+    book(){
+    cy.get(elements.firstName).type("Limakatso");
+    cy.get(elements.lastName).type("Ntoba");
+    cy.get(elements.price).type("3800");
+
+    //deposit drop down
+    cy.get(elements.depo)
+    .select("true")
+    .invoke("val")
+    .should("eq", "true");
+
+    cy.get(elements.checkIn).type("2023-04-03");
+    cy.get(elements.checkOut).type("2023-04-10");
+    cy.get(elements.saveBTN).click();
+
+    cy.wait(1000);
+
+    cy.get(elements.row).its("length").should("be.gte", 1);
+    }
+
+    //delete the booking
+    deleteBooking(){
+        cy.get(elements.delete).each(($el, index, $list) => {
+            cy.get($el).click({ multiple: true });
+    })
+    
+
+        }
+    }
